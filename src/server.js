e6ea5eeb06e226434467e86e4a3a3f7cab88f583
Kreal244 <nuunkhang@gmail.com>
2023-05-24 12:21:52 +0700

    remove socketio

--- src/server.js
+++ src/server.js
@@ -7,9 +7,6 @@ import orderRoute from "./Router/orderRoute.js";
 import chatRoute from "./Router/chatRoute.js";
 import cors from "cors";
 import { Server } from "socket.io";
-import { addUser, getUser, getUsersInRoom, removeUser } from "./user.js";
-import Chat from "./models/chatModel.js";
-import { Db } from "mongodb";
 import authenRoute from "./Router/authenRoute.js";
 import resetRoute from "./Router/repasswordRoute.js";
 dotenv.config();
@@ -48,49 +45,5 @@ const port = process.env.PORT || 5000;
 const httpServer = app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
 });
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-  transports: ["polling", "websocket"],
-});
 
-io.on("connect", (socket) => {
-  socket.on("join", (account) => {
-    const { user } = addUser(socket.id, account);
-    Chat.findOne({ roomId: user.room }, (err, room) => {
-      if (!room) {
-        Chat.create({
-          roomId: user.room,
-          messages: [
-            {
-              name: "DKL Store",
-              email: "admin",
-              message:"Welcome to DKL store!",
-            },
-          ],
-        });
-      }
-    });
 
-    socket.join(user.room);
-  });
-
-  socket.on("sendMessage", ({ message }) => {
-    const user = getUser(socket.id);
-    const data = { user: user.name, email: user.email, message };
-    io.to(user.room).emit("message", data);
-    Chat.findOne({ "chats.roomId": user.room }, (err, room) => {
-      room.messages.push(data);
-      room.save(function (err) {
-        if (err) return handleError(err);
-        console.log("Success!");
-      });
-    });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("disconnect");
-  });
-});
